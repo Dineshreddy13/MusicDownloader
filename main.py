ca8ebcf13858014db97d14f1fc9308ebb00ac837
Dinesh reddy <dineshreddy.bijjam@gmail.com>
2025-08-24 22:23:56 +0530

    remove elapsed time in the main file

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ from components.SongDownload import MusicDownloader
 
 def main():
     songs= []
-    start = time.time()
     downloader = MusicDownloader()
     if(len(songs) == 0):
         video_url = input("Enter the YouTube video URL: ").strip()
         songs = video_url.split(" ")
     for i in songs:
         downloader.process(i)
-    end = time.time()
-    elapsed = end - start
-    elapsed = round(end - start)
-    print("Elapsed time : ", elapsed//60 ,"min", elapsed%60, "sec")
 if __name__ == "__main__":
     main()
