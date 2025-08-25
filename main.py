cd025433a385ed3b1de7f7a41e4402ba756a0fd4
Dinesh reddy <dineshreddy.bijjam@gmail.com>
2025-08-25 21:55:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import time
 from components.SongDownload import MusicDownloader
 
 def main():
-    songs= []
     downloader = MusicDownloader()
-    if(len(songs) == 0):
-        video_url = input("Enter the YouTube video URL: ").strip()
-        songs = video_url.split(" ")
-    for i in songs:
-        downloader.process(i)
+    url = input("Enter the URL : ").strip()
+    downloader.process(url)
 if __name__ == "__main__":
     main()
