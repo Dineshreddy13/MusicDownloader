bbd2b1ccc04ded9a948c85e9a84cfa0923420cae
Dinesh reddy <dineshreddy.bijjam@gmail.com>
2025-08-29 14:16:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from components.SongDownload import MusicDownloader
 
 def main():
+    songs= []
     downloader = MusicDownloader()
-    url = input("Enter the URL : ").strip()
-    downloader.process(url)
+    if(len(songs) == 0):
+        video_url = input("Enter the URL: ").strip()
+        songs = video_url.split(" ")
+    for i in songs:
+        downloader.process(i)
 if __name__ == "__main__":
     main()
